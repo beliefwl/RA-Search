554f5eb0179e438fd400a1969ac60294ec2fb49f
wenglin <beliefwl@163.com>
2019-07-02 19:41:16 +0800

    删除无用文件

--- hello.py
+++
@@ -1,8 +0,0 @@
-
-
-import  urllib2
-
-
-url = 'http://mobile.cfda.gov.cn/datasearch/QueryList?pageIndex=1&pageSize=1&tableId=26'
-html = urllib2.urlopen(url).read()
-print html.decode("gbk").encode("utf-8")
